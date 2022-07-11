8ac88dd55b82a3c3e3364c464e401c4cc045fe36
Breno Nascimento <satsukebare23@gmail.com>
2022-07-11 13:52:44 -0300

    criando pages signIn/Home

---
+++ src/Pages/SignIn/SignIn.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SignIn extends React.Component {
+  render() {
+    return (
+      <h1>
+        Area de LogIn
+      </h1>
+    )
+  }
+}
+
+export default SignIn;
