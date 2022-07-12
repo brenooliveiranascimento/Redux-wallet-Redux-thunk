8e7d8d9c90c4cba1dc05c1fdb6f172467e1d20a8
Breno Nascimento <satsukebare23@gmail.com>
2022-07-12 12:17:04 -0300

    conectando cifirebase

--- src/Pages/SignIn/SignIn.tsx
+++ src/Pages/SignIn/SignIn.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { mapStateTypes } from '../../globalTypes/mapStateTypes';
+import firebase from '../../Services/firebase_connection';
 
 class SignIn extends React.Component {
-  
+
   render() {
     return (
       <h1>
---
+++ src/Services/firebase_connection.ts
@@ -0,0 +1,19 @@
+import firebase from 'firebase';
+import "firebase/database";
+import "firebase/auth";
+import "firebase/firestore";
+
+let firebaseConfig = {
+  apiKey: "AIzaSyCwQlNv_5-jiEDvuZkvust82ZFCdV-MkAM",
+  authDomain: "monthly-maneger.firebaseapp.com",
+  projectId: "monthly-maneger",
+  storageBucket: "monthly-maneger.appspot.com",
+  messagingSenderId: "747771561804",
+  appId: "1:747771561804:web:22e97e3bc944d6217c31e9"
+};
+  
+  if(!firebase.apps.length){
+      firebase.initializeApp(firebaseConfig)
+  }
+
+  export default firebase
