21371e0ac4a2be632494086003dc0142a0f8d599
Breno Nascimento <satsukebare23@gmail.com>
2022-07-12 12:20:15 -0300

    create authForm

---
+++ src/Components/Forms/AuthForm/AuthForm.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class AuthForm extends React.Component {
+  render() {
+    return (
+      <h1>
+        Formulario de login
+      </h1>
+    )
+  }
+}
+
+export default AuthForm;
