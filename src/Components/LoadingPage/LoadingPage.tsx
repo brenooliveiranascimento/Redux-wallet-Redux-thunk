6f7063e69b081694d4a23c2e2893c499239adf88
Breno Nascimento <satsukebare23@gmail.com>
2022-07-14 21:45:27 -0300

    add loadingPage

---
+++ src/Components/LoadingPage/LoadingPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './LoadingPage.css';
+
+function LoadingPage() {
+  return (
+    <section
+      className="containerLoading"
+    >
+      <h1>
+        Carregando...
+      </h1>
+    </section>
+  );
+}
+
+export default LoadingPage;
--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import LoadingPage from '../Components/LoadingPage/LoadingPage';
 import AppRoutes from './AppRoutes/AppRoutes';
 import AuthRoutes from './AuthRoutes/AuthRoutes';
 
 function Routes() {
   const userData = useSelector((state: any) => state.userReducer.userData.name).length;
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(false);
+  }, []);
+
+  if (loading) {
+    return (
+      <LoadingPage />
+    );
+  }
 
   return (
     userData === 0 ? <AuthRoutes /> : <AppRoutes />
